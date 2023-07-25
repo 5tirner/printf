71e23ce99af59004b2e00ceabb8cb5c188bd2b3a
Zakaria Sabri <zakariasabri2001@gmail.com>
2023-07-25 14:52:48 -0700

    add %p and fix it

--- p_handle.c
+++ p_handle.c
@@ -11,7 +11,12 @@ int	p_handle(unsigned int p)
 {
 	int	len;
 
-	len = write(1, "0x", 2);
-	len += xX_handle(p, 'x');
+	if (p == 0)
+		len = write(1, "(nil)", 5);
+	else
+	{
+		len = write(1, "0x", 2);
+		len += xX_handle(p, 'x');
+	}
 	return (len);
 }
